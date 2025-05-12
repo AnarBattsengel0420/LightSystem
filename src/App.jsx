2d5bb0a076492615bed5fcd3e7959b2db53e32b6
Anar <s21c076b@nmct.edu.mn>
2025-05-12 17:17:58 +0800

    aldaa zasav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
-import Dashboard from "./Dashboard";
+import Dashboard from "./dashboard";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import { auth } from "./firebase";
